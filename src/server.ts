902cd01b9f7e5f4077e292aa9f262888c009f503
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-02-09 02:42:03 +0300

    Add morgan - HTTP request logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 
 const app: Express = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
