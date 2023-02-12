82bdb8044e0ed1c93de8bdb1ecdc730a9fc896cf
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-02-12 15:07:22 +0300

    Create model Movie

---
+++ src/models/movies.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+interface IMovie {
+  title: string;
+  type: 'movie' | 'tvSeries';
+  directors: string[];
+  genres: string[];
+  countries: string[];
+  year: number;
+  description: string;
+}
+
+const movieSchema = new mongoose.Schema<IMovie>(
+  {
+    title: { type: String, required: true },
+    type: { type: String, required: true },
+    directors: { type: [String], required: true },
+    genres: { type: [String], required: true },
+    countries: { type: [String], required: true },
+    year: { type: Number, required: true },
+    description: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Movie = mongoose.model<IMovie>('Movie', movieSchema);
+
+export default Movie;
